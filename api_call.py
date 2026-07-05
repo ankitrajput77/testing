54e822855a9cf23d835ac69502c5ead64a533513
ankitrajput77 <r.ankit@iitg.ac.in>
2026-07-05 11:03:05 +0530

    API Call

---
+++ api_call.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+from google import genai
+
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.environ.get("GEMINI_API_KEY"),
+)
+
+tools = [
+    {
+        'type': 'google_search',
+    },
+]
+
+generation_config = {
+    'temperature': 1,
+    'max_output_tokens': 65536,
+    'top_p': 0.95,
+    'thinking_level': 'high',
+}
+
+prompt = "Explain transformers in simple words."
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash-lite",
+    contents=prompt
+)
+
+with open("output.txt", "w") as file:
+    file.write(prompt)
+    file.write(response.text)
+
+
+
+
